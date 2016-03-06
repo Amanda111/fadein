7eb3f8cf83848bcec33aeeca00b627401585247e
github <631551773@qq.com>
2016-03-06 21:02:05 +0800

    update

--- ajaxmethod.js
+++ ajaxmethod.js
@@ -4,15 +4,15 @@ $(function(){
 		url:"origin.json",
 		dataType:"json",
 		success:function(data){
-			var height=0,
 			html='';
 			html+='<div class="items"><h3>'+data[0].year+'</h3></div><div class="items"><h3>'+data[0].tag+'</h3></div>';
 			for(var i=1;i<data.length;i++){
-				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<h4>'+data[i].name+'</h4></div>';
+				html+='<h4 class="items"><span class="date">'+data[i].date+'</span><span>'+data[i].name+'</span></h4>';
 			}
-			$("body").html(html);
+			$(".line").height(i*45).css("display","none").slideDown(500);
+			$(".content").html(html);
 			$(".items").each(function(index){
-				$(this).delay(300*index).fadeIn(800);
+				$(this).delay(300*index).addClass("on").fadeIn(800);
 			});
 		}
 	});
